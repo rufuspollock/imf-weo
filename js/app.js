ec201081ca2ec4a86b584d906692de8f864ab73d
Rufus Pollock <rufus.pollock@okfn.org>
2014-09-13 18:37:09 +0100

    [app][m]: working load of a data series from CKAN API.

--- js/app.js
+++ js/app.js
@@ -1,16 +1,32 @@
-jQuery(document).ready(function($) {
-  console.log('here');
+var ckanEndpoint = 'http://localhost:5000';
+var valuesResourceId = '8b9a25aa-0b79-4aef-9c39-00418935bc37';
 
+jQuery(document).ready(function($) {
   setupCountries();
   setupIndicators();
 
   bindEvents();
+
+  getData({Indicator: 'BCA', Country: 'GBR'}, function(err, data) {
+    console.log(data);
+  });
 });
 
 var setupCountries = function() {};
 var setupIndicators = function() {};
 
-var getData = function(series) {
+// series look like: [ [indicator, country], [indicator, country] ]
+// maybe just get indicators and filter the country (??)
+// worry about efficiency later
+var getData = function(series, cb) {
+  var ckan = new CKAN.Client(ckanEndpoint);
+  var queryObj = {
+    resource_id: valuesResourceId,
+    filters: series
+  };
+  ckan.action('datastore_search', queryObj, function(err, data) {
+    cb(err, data.result.records);
+  });
 };
 
 var bindEvents = function() {
