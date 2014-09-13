ba19c733dd3024f29107a49e7d54c5c54ea02acb
Rufus Pollock <rufus.pollock@okfn.org>
2014-09-14 00:39:12 +0100

    [app][l]: basic grid and graph working and you can add series.

--- js/app.js
+++ js/app.js
@@ -1,14 +1,25 @@
 var ckanEndpoint = 'http://localhost:5000';
 var valuesResourceId = '8b9a25aa-0b79-4aef-9c39-00418935bc37';
 var Data = {
-  cache: {}
+  cache: {},
+  rawDataset: {
+    fields: [
+      { id: 'year', type: 'number' }
+    ],
+    records: []
+  }
 };
 
 jQuery(document).ready(function($) {
+  for(var xx=0; xx<40; xx++) {
+    Data.rawDataset.records.push({year: 1980+xx});
+  }
   setup(function() {
     $('.chosen-select').chosen();
     $('form').submit(onAdd);
+    // WEO starts in 1980 and current runs until 2019
   });
+  addSeries({Indicator: 'LUR', Country: 'GBR'});
 });
 
 function onAdd(e) {
@@ -22,11 +33,58 @@ function onAdd(e) {
   if (!(country && indicator)) {
     return;
   }
+  var series = {Indicator: indicator, Country: country};
+  addSeries(series);
+}
 
-  getData({Indicator: indicator, Country: country}, function(err, data) {
-    console.log(data);
-    Data.cache[indicator] 
+function addSeries(series) {
+  // todo: check cache first
+  getData(series, function(err, data) {
+    // cache the data
+    if (!(series.Indicator in Data.cache)) {
+      Data.cache[series.Indicator] = {};
+    }
+    Data.cache[series.Indicator][series.Country] = data;
+
+    var fieldId = series.Indicator + '::' + series.Country;
+    Data.rawDataset.fields.push({id: fieldId, type: 'number'});
+    _.each(data, function(row) {
+      var idx = row.Year - 1980;
+      Data.rawDataset.records[idx][fieldId] = row.Value;
+    });
+    // this is getting sort of ugly in terms of nesting
+    render();
   });
+}
+
+var render = function() {
+  var dataset = new recline.Model.Dataset({
+    fields: Data.rawDataset.fields,
+    records: Data.rawDataset.records
+  });
+
+  var $el = $('.grid-holder');
+  $el.empty();
+  var grid = new recline.View.SlickGrid({
+    model: dataset,
+    el: $el
+  });
+  grid.visible = true;
+  grid.render();
+
+  var $el = $('.flot-holder');
+  $el.empty();
+  var graphSeries = _.pluck(Data.rawDataset.fields.slice(1), 'id');
+  var graph = new recline.View.Graph({
+    model: dataset,
+    state: {
+      group: "year",
+      series: graphSeries
+    }
+  });
+  $el.append(graph.el);
+  graph.render();
+  graph.redraw();
 }
 
 var setup = function(cb) {
@@ -58,7 +116,7 @@ var setup = function(cb) {
     _.each(data, function(ind) {
       var $option = $('<option />')
         .attr('value', ind.id)
-        .text(ind.title)
+        .text(ind.title + ' [' + ind.id + ']')
         ;
       $select.append($option);
     });
